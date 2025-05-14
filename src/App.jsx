1a8c767a07c4ec21e6bb9d81a2ebf3672b4bc2ad
gamarradiego <diegolgamarra@gmail.com>
2025-05-14 20:43:06 -0300

    rutas agregadas en app()

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes, Navigate } from "react-router-dom";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {/* you can use 👇 character (* or wildcard) to any other route
+        in combination with the Navigate component you can redirect the user to a default page */}
+        <Route path="/*" element={<Navigate replace to="/login" />} />
+
+          
+        {/* </Route> */}
+        
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/home" element={<HomePage />}/>
+        <Route path="/quiz:id" element={<QuizPage />} />
+        <Route path="/quiz:id/question:id" element={<QuestionPage />} />
+      </Routes>
     </>
   )
 }
