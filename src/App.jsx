de91d1ef8ed6584392a12e3c391f8ec9febfbb33
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2025-05-19 18:45:36 -0300

    Ediciones en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
+import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 
 import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import QuizPage from "./pages/QuizPage";
-import QuestionPage from "./pages/QuestionPage";
+// import HomePage from "./pages/HomePage";
+// import QuizPage from "./pages/QuizPage";
+// import QuestionPage from "./pages/QuestionPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate replace to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/quiz/:id" element={<QuizPage />} />
-      <Route
+      {/* <Route path="/home" element={<HomePage />} />
+      <Route path="/quiz/:id" element={<QuizPage />} /> */}
+      {/* <Route
         path="/quiz/:quizId/question/:questionId"
         element={<QuestionPage />}
-      />
+      /> */}
       <Route path="*" element={<Navigate replace to="/login" />} />
     </Routes>
   );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+  return (
+    <div className="login-page">
+      <h1>Login Page</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
